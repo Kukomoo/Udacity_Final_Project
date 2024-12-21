47852c8936372bdc6fba1c3d659a6a2bcea705a9
Nada Khas <122938829+Kukomoo@users.noreply.github.com>
2024-12-21 17:47:18 +0200

    Update formHandler.js

--- starter_project/src/client/js/formHandler.js
+++ starter_project/src/client/js/formHandler.js
@@ -7,25 +7,25 @@ function initFormHandler() {
     }
 }
 
-
 function handleSubmit(event) {
     event.preventDefault();
 
     const formText = document.getElementById('name').value;
-
 
     if (!formText) {
         alert("Please enter text or a URL!");
         return;
     }
 
-
-    if (!isValidURL(formText)) {
-        alert("Please enter a valid URL!");
+    
+    try {
+        checkForName(formText);
+    } catch (error) {
+        console.error('Validation error:', error);
         return;
     }
 
-
+    
     fetch('http://localhost:8000/analyze', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
@@ -33,7 +33,6 @@ function handleSubmit(event) {
     })
         .then((response) => response.json())
         .then((data) => {
-
             const resultsElement = document.getElementById('results');
             resultsElement.innerHTML = `
                 <p><strong>Polarity:</strong> ${data.score_tag}</p>
@@ -47,9 +46,4 @@ function handleSubmit(event) {
         });
 }
 
-function isValidURL(inputText) {
-    const urlRegex = /^(https?:\/\/)?([\w-]+\.)+[a-z]{2,}(\/[\w-./?%&=]*)?$/i;
-    return urlRegex.test(inputText);
-}
-
-export { handleSubmit, isValidURL, initFormHandler };
+export { handleSubmit, initFormHandler };
